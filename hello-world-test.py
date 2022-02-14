4db17af776412f6a47757217cc91e73d684bf08e
NMTAFE20030249-KMAKKB <99634595+NMTAFE20030249-KMAKKB@users.noreply.github.com>
2022-02-14 14:11:06 +0800

    Add files via upload

---
+++ hello-world-test.py
@@ -0,0 +1,5 @@
+Python 3.7.5 (tags/v3.7.5:5c02a39a0b, Oct 15 2019, 00:11:34) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("Hello World")
+Hello World
+>>> 
